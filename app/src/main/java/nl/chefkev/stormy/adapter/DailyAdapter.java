64c621ee6d6a05ef6a7d48b736f2646b153fa301
Kevin <kevin.verbeek@flitsmeister.nl>
2019-02-21 12:13:37 +0100

    more work

--- app/src/main/java/nl/chefkev/stormy/adapter/DailyAdapter.java
+++ app/src/main/java/nl/chefkev/stormy/adapter/DailyAdapter.java
@@ -2,6 +2,7 @@ package nl.chefkev.stormy.adapter;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -11,34 +12,52 @@ import nl.chefkev.stormy.R;
 import nl.chefkev.stormy.weather.Day;
 import nl.chefkev.stormy.databinding.DailyListItemBinding;
 
-public class DailyAdapter {
+public class DailyAdapter extends RecyclerView.Adapter<DailyAdapter.ViewHolder>
+{
 
     private List<Day> days;
     private Context context;
 
-    public DailyAdapter(List<Day> days, Context context) {
+    public DailyAdapter(List<Day> days, Context context)
+    {
         this.days = days;
         this.context = context;
     }
 
-    public DailyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    @Override
+    public DailyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
 
         DailyListItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.daily_list_item,
                 parent, false);
 
-        return new ViewHolder();
+        return new ViewHolder(binding);
     }
 
-    public void onBindViewHolder(DailyAdapter.ViewHolder holder, int position) {
+    @Override
+    public void onBindViewHolder(DailyAdapter.ViewHolder holder, int position)
+    {
         Day day = days.get(position);
         holder.listItemBinding.setDay(day);
+
     }
 
+    @Override
     public int getItemCount() {
         return days.size();
     }
 
-    public class ViewHolder {
+    public class ViewHolder extends  RecyclerView.ViewHolder
+    {
         public DailyListItemBinding listItemBinding;
+
+
+        public ViewHolder(DailyListItemBinding dailyLayoutItemBinding)
+        {
+            super(dailyLayoutItemBinding.getRoot());
+            listItemBinding = dailyLayoutItemBinding;
+        }
+
+
     }
 }
---
+++ app/src/main/java/nl/chefkev/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,37 @@
+package nl.chefkev.stormy.ui;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import nl.chefkev.stormy.R;
+import nl.chefkev.stormy.adapter.DailyAdapter;
+import nl.chefkev.stormy.databinding.ContentDailyForecastBinding;
+import nl.chefkev.stormy.weather.Day;
+
+public class DailyForecastActivity extends AppCompatActivity
+{
+
+    private ContentDailyForecastBinding binding;
+    private DailyAdapter dailyAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        setContentView(R.layout.activity_daily_forecast);
+        List<Day> dayList = (ArrayList<Day>) intent.getSerializableExtra("dailyList");
+
+        binding = DataBindingUtil.setContentView(this, R.layout.content_daily_forecast);
+
+        dailyAdapter = new DailyAdapter(dayList, this );
+
+        binding.dailyListItems.setAdapter(dailyAdapter);
+        binding.dailyListItems.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
